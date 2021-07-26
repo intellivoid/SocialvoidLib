01fd6833f077429563742d93458a89978a66d62d
Zi Xing <zixing.narrakas1996@gmail.com>
2021-07-26 11:33:44 -0400

    Added the ability to create a session via the RPC server, added python examples

---
+++ tests/client_tests/authenticate_user.py
@@ -0,0 +1,20 @@
+import os
+import json
+from jsonrpcclient import request
+
+with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'client_info.json'), 'r') as f:
+    client_info = json.load(f)
+
+response = request(
+    client_info["endpoint"], "session.create",
+     public_hash=client_info["public_hash"],
+     private_hash=client_info["private_hash"],
+     platform=client_info["platform"],
+     name=client_info["name"],
+     version=client_info["version"]
+)
+
+print(response.data.result)
+
+with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'session.social_session'), 'w') as f:
+    json.dump(response.data.result, f)
