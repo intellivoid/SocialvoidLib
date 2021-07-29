cd0bf9104acf48d5c66501b91326b733276d7699
Zi Xing <zixing.narrakas1996@gmail.com>
2021-07-29 07:26:19 -0400

    Updated sessions.sql

--- database/sessions.sql
+++ database/sessions.sql
@@ -9,28 +9,24 @@
 
 create table if not exists sessions
 (
-    id                         int auto_increment comment 'The Unique Internal Database ID',
-    public_id                  varchar(255) null comment 'The Unique Public ID for this session',
+    id                         varchar(255) not null comment 'The Unique Public ID for this session',
     flags                      blob         null comment 'ZiProto encoded array of flags associated with this session',
     authenticated              tinyint(1)   null comment 'Indicates if this session is currently authenticated or not',
     user_id                    int          null comment 'The User ID that owns this session',
     authentication_method_used varchar(64)  null comment 'The Authentication Method used create this session',
-    device_model               varchar(255) null comment 'The device model used to create this session',
     platform                   varchar(255) null comment 'The platform that created this session',
-    system_version             varchar(255) null comment 'The system version used to create this session',
     client_name                varchar(255) null comment 'The name of the client using this session',
     client_version             varchar(255) null comment 'The version of the client being used',
     ip_address                 varchar(255) null comment 'The last known IP address to use this session',
-    session_cache              blob         null comment 'Session Cache Data, ZiProto encoded',
-    session_data               blob         null comment 'Session Data, ZiProto encoded',
+    data                       blob         null comment 'Session Data, ZiProto encoded',
+    security                   blob         null comment 'The security details of the session',
     last_active_timestamp      int          null comment 'The Unix Timestamp for when this session was last active',
     created_timestamp          int          null comment 'The Unix Timestmap for when this session was created',
-    constraint sessions_id_uindex
-        unique (id),
+    expires_timestamp          int          null comment 'The Unix Timestamp for when this session expires',
     constraint sessions_public_id_uindex
-        unique (public_id),
+        unique (id),
     constraint sessions_public_id_user_id_uindex
-        unique (public_id, user_id),
+        unique (id, user_id),
     constraint sessions_users_id_fk
         foreign key (user_id) references users (id)
 )
