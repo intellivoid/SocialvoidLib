da5a13e2c2aa4c77d0db1d797f689ac59081ed28
Zi Xing <zixing.narrakas1996@gmail.com>
2021-08-29 20:10:32 -0400

    Added default message to \SocialvoidLib\Exceptions\Standard\Validation > InvalidClientNameException

--- src/SocialvoidLib/Exceptions/Standard/Validation/InvalidClientNameException.php
+++ src/SocialvoidLib/Exceptions/Standard/Validation/InvalidClientNameException.php
@@ -24,7 +24,7 @@
          * @param string $message
          * @param Throwable|null $previous
          */
-        public function __construct($message = "", Throwable $previous = null)
+        public function __construct($message = "The given client name is invalid", Throwable $previous = null)
         {
             parent::__construct($message, StandardErrorCodes::InvalidClientNameException, $previous);
             $this->message = $message;
