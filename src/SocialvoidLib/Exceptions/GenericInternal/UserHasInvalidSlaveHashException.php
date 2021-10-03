36eb6087d6f0cbd79d7a4edb5536521c3dcd666b
Netkas <zixing.narrakas1996@gmail.com>
2021-10-02 21:28:57 -0400

    Added UserHasInvalidSlaveHashException to SocialvoidLib\Exceptions\GenericInternal

---
+++ src/SocialvoidLib/Exceptions/GenericInternal/UserHasInvalidSlaveHashException.php
@@ -0,0 +1,21 @@
+<?php
+
+    namespace SocialvoidLib\Exceptions\GenericInternal;
+
+    use Exception;
+    use Throwable;
+
+    class UserHasInvalidSlaveHashException extends Exception
+    {
+        /**
+         * @param string $message
+         * @param int $code
+         * @param Throwable|null $previous
+         */
+        public function __construct($message = "", $code = 0, Throwable $previous = null)
+        {
+            parent::__construct($message, $code, $previous);
+            $this->message = $message;
+            $this->code = $code;
+        }
+    }
