924eeefda5eca506eab1a0ceb15c1ba12b0e8762
Netkas <zixing.narrakas1996@gmail.com>
2021-10-07 13:09:20 -0400

    Added alternative multi-part parser for CDN (Useless)

--- src/cdn_frontend/index.php
+++ src/cdn_frontend/index.php
@@ -20,6 +20,50 @@
     import("net.intellivoid.socialvoidlib");
 
     /**
+     * @return array
+     */
+    function parseRawHttpBoy(): array
+    {
+        // read incoming data
+        $input = file_get_contents('php://input');
+
+
+        // grab multipart boundary from content type header
+        preg_match('/boundary=(.*)$/', $_SERVER['CONTENT_TYPE'], $matches);
+        $boundary = $matches[1];
+
+        // split content by boundary and get rid of last -- element
+        $a_blocks = preg_split("/-+$boundary/", $input);
+        array_pop($a_blocks);
+        $a_data = [];
+
+        // loop data blocks
+        foreach ($a_blocks as $id => $block)
+        {
+            if (empty($block))
+                continue;
+
+            // you'll have to var_dump $block to understand this and maybe replace \n or \r with a visibile char
+
+            // parse uploaded files
+            if (strpos($block, 'application/octet-stream') !== FALSE)
+            {
+                // match "name", then everything after "stream" (optional) except for prepending newlines
+                preg_match("/name=\"([^\"]*)\".*stream[\n|\r]+([^\n\r].*)?$/s", $block, $matches);
+            }
+            // parse all other fields
+            else
+            {
+                // match "name" and optional value in between newline sequences
+                preg_match('/name=\"([^\"]*)\"[\n|\r]+([^\n\r].*)?\r$/s', $block, $matches);
+            }
+            $a_data[$matches[1]] = $matches[2];
+        }
+
+        return $a_data;
+    }
+
+    /**
      * Returns an HTTP parameter
      *
      * @param string $name
@@ -47,9 +91,11 @@
                         return $entityBodyDecoded[$name];
                 }
             }
-
         }
 
+        $parsedRaw = parseRawHttpBoy();
+        if(isset($parsedRaw[$name]))
+            return $parsedRaw[$name];
         return null;
     }
 
