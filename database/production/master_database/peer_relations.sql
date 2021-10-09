4f69111ec20ce23288b0a874aa15010e1668ecbd
Netkas <zixing.narrakas1996@gmail.com>
2021-10-09 17:44:43 -0400

    Updated peer_relations.sql

--- database/production/master_database/peer_relations.sql
+++ database/production/master_database/peer_relations.sql
@@ -1,10 +1,10 @@
 create table peer_relations
 (
+    user_id                int    not null comment 'The User ID that is responsible for the initiation',
+    target_user_id         int    not null comment 'The target user ID that the user is following',
     state                  int(2) null comment 'The current state of the following condition',
     created_timestamp      int    null comment 'The Unix Timestamp for when this record was created',
     last_updated_timestamp int    null comment 'The Unix Timestamp of when this record was last updated',
-    target_user_id         int    not null comment 'The target user ID that the user is following',
-    user_id                int    not null comment 'The User ID that is responsible for the initiation',
     constraint relations_user_id_target_user_id_uindex
         unique (user_id, target_user_id)
 )
