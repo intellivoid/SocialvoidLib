5341ca1e1699bc39f42e1e42f23eea78a3a4ea72
Netkas <zixing.narrakas1996@gmail.com>
2021-10-09 17:40:07 -0400

    Added peer_timelines.sql (slave)

---
+++ database/production/master_database/peer_timelines.sql
@@ -0,0 +1,15 @@
+create table socialvoid_slave.peer_timelines
+(
+    user_id                int        not null comment 'The User ID that owns this timeline',
+    post_chunks            mediumblob null comment 'ZiProto encoded data of the Timeline post chunks',
+    new_posts              int        null comment 'The number of posts that has been added to this timeline',
+    last_updated_timestamp int        null comment 'The Unix Timestamp for when this record was last updated',
+    created_timestamp      int        null comment 'The Unix Timestamp for when this record was created',
+    constraint user_timelines_user_id_uindex
+        unique (user_id)
+)
+    comment 'Data for housing personalized Timelines for users';
+
+alter table socialvoid_slave.peer_timelines
+    add primary key (user_id);
+
