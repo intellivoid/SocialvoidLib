b04a8c07e5567838fdb94b5e39fb64523f00ff8a
Netkas <zixing.narrakas1996@gmail.com>
2021-10-12 08:42:06 -0400

    Added Cursor object

--- src/SocialvoidLib/Objects/Cursor.php
+++ src/SocialvoidLib/Objects/Cursor.php
@@ -7,7 +7,70 @@
     class Cursor
     {
         /**
+         *
+         *
          * @var int
          */
         public $ContentLimit;
+
+        /**
+         *
+         * @var int
+         */
+        public $Cursor;
+
+        /**
+         * Calculates the offset from the current cursor
+         *
+         * @return int
+         */
+        public function getOffset(): int
+        {
+            $offset = 0;
+            for ($k = 0 ; $k < ($this->Cursor < 0 ? 1 : $this->Cursor); $k++)
+                $offset += $this->ContentLimit;
+            return $offset;
+        }
+
+        /**
+         * @param int $content_limit
+         * @param int $cursor
+         */
+        public function __construct(int $content_limit=100, int $cursor=1)
+        {
+            $this->ContentLimit = $content_limit;
+            $this->Cursor = $cursor;
+        }
+
+        /**
+         * Returns an array representation of the object
+         *
+         * @return array
+         */
+        public function toArray(): array
+        {
+            return [
+                'content_limit' => $this->ContentLimit,
+                'cursor' => $this->Cursor
+            ];
+        }
+
+        /**
+         * Constructs an object from an array representation
+         *
+         * @param array $data
+         * @return Cursor
+         */
+        public static function fromArray(array $data): Cursor
+        {
+            $CursorObject = new Cursor();
+
+            if(isset($data['content_limit']))
+                $CursorObject->ContentLimit = $data['content_limit'];
+
+            if(isset($data['cursor']))
+                $CursorObject->Cursor = $data['cursor'];
+
+            return $CursorObject;
+        }
     }
