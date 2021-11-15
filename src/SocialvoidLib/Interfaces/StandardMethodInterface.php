c3b5190fb0474cae735d6dab4b73497b6453db42
Netkas <zixing.narrakas1996@gmail.com>
2021-11-15 16:29:24 -0500

    Updated interfaces

--- src/SocialvoidLib/Interfaces/StandardMethodInterface.php
+++ src/SocialvoidLib/Interfaces/StandardMethodInterface.php
@@ -4,59 +4,66 @@
 
     use SocialvoidLib\Abstracts\Flags\PermissionSets;
     use SocialvoidLib\Objects\Standard\MethodDefinition;
-    use SocialvoidLib\Objects\Standard\ObjectDefinition;
     use SocialvoidLib\Objects\Standard\ParameterDefinition;
+    use SocialvoidLib\Objects\Standard\TypeDefinition;
 
     interface StandardMethodInterface
     {
-        /**
-         * Returns the name of the method
-         *
-         * @return string
-         */
-        public static function getName(): string;
-
         /**
          * Returns the namespace that the method is under
          *
          * @return string
          */
-        public static function getNamespace(): string;
+        public static function getStandardNamespace(): string;
 
         /**
          * Returns name of the method without the leading namespace
          *
          * @return string
          */
-        public static function getMethodName(): string;
+        public static function getStandardMethodName(): string;
 
         /**
          * Returns the full name of the method with the leading namespace
          *
          * @return string
          */
-        public static function getMethod(): string;
+        public static function getStandardMethod(): string;
 
         /**
          * Returns a description of the method
          *
          * @return string
          */
-        public static function getDescription(): string;
+        public static function getStandardDescription(): string;
 
         /**
          * Returns all the parameters that this method accepts
          *
          * @return ParameterDefinition[]
          */
-        public static function getParameters(): array;
+        public static function getStandardParameters(): array;
 
         /**
          * Returns an array of permission requirements
          *
          * @return array|PermissionSets[]|string[]
          */
-        public static function getPermissionRequirements(): array;
+        public static function getStandardPermissionRequirements(): array;
+
+        /**
+         * Returns an array of possible error codes that this method may return
+         *
+         * @return int[]
+         */
+        public static function getStandardPossibleErrorCodes(): array;
+
+        /**
+         * Returns an array of types that this method can potentially return
+         *
+         * @return array|TypeDefinition
+         */
+        public static function getReturnTypes(): array;
 
         /**
          * Returns a method definition object of the standard object
