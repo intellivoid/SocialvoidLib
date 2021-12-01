91de80731cd05edefd7a01d41eb51ef596be6da0
Netkas <zixing.narrakas1996@gmail.com>
2021-12-01 18:25:43 -0500

    Implemented StandardErrorInterface to CaptchaAnswerNotApplicable \SocialvoidLib\Exceptions\Standard > Security

--- src/SocialvoidLib/Exceptions/Standard/Security/CaptchaAnswerNotApplicableException.php
+++ src/SocialvoidLib/Exceptions/Standard/Security/CaptchaAnswerNotApplicableException.php
@@ -4,9 +4,11 @@
 
     use Exception;
     use SocialvoidLib\Abstracts\StandardErrorCodes;
+    use SocialvoidLib\Interfaces\StandardErrorInterface;
+    use SocialvoidLib\Objects\Standard\ErrorDefinition;
     use Throwable;
 
-    class CaptchaAnswerNotApplicableException extends Exception
+    class CaptchaAnswerNotApplicableException extends Exception implements StandardErrorInterface
     {
         /**
          * @param string $message
@@ -15,5 +17,37 @@
         public function __construct($message = "The captcha cannot be answered directly", Throwable $previous = null)
         {
             parent::__construct($message, StandardErrorCodes::CaptchaAnswerNotApplicableException, $previous);
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getDefinition(): ErrorDefinition
+        {
+            return new ErrorDefinition(self::getName(), self::getDescription(), self::getErrorCode());
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getName(): string
+        {
+            return 'CaptchaAnswerNotApplicable';
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getDescription(): string
+        {
+            return 'Raised when the captcha does not accept answers from the client';
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getErrorCode(): int
+        {
+            return StandardErrorCodes::CaptchaAnswerNotApplicableException;
         }
     }
