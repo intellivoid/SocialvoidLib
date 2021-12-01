e7ca732b4fc70492693db0efaeca08700061745f
Netkas <zixing.narrakas1996@gmail.com>
2021-12-01 18:24:04 -0500

    Implemented StandardErrorInterface to CaptchaAlreadyUsedException \SocialvoidLib\Exceptions\Standard > Security

--- src/SocialvoidLib/Exceptions/Standard/Security/CaptchaAlreadyUsedException.php
+++ src/SocialvoidLib/Exceptions/Standard/Security/CaptchaAlreadyUsedException.php
@@ -4,9 +4,11 @@
 
     use Exception;
     use SocialvoidLib\Abstracts\StandardErrorCodes;
+    use SocialvoidLib\Interfaces\StandardErrorInterface;
+    use SocialvoidLib\Objects\Standard\ErrorDefinition;
     use Throwable;
 
-    class CaptchaAlreadyUsedException extends Exception
+    class CaptchaAlreadyUsedException extends Exception implements StandardErrorInterface
     {
         /**
          * @param string $message
@@ -15,5 +17,37 @@
         public function __construct($message = "Cannot use this captcha instance as it has already been used", Throwable $previous = null)
         {
             parent::__construct($message, StandardErrorCodes::CaptchaAlreadyUsedException, $previous);
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getDefinition(): ErrorDefinition
+        {
+            return new ErrorDefinition(self::getName(), self::getDescription(), self::getErrorCode());
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getName(): string
+        {
+            return 'CaptchaAlreadyUsed';
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getDescription(): string
+        {
+            return 'Raised when the requested captcha has already been used';
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getErrorCode(): int
+        {
+            return StandardErrorCodes::CaptchaAlreadyUsedException;
         }
     }
