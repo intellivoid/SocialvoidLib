bbef20153f95267d5a7e507fa2b4dcaef18a0645
Netkas <zixing.narrakas1996@gmail.com>
2021-12-01 18:21:45 -0500

    Implemented StandardErrorInterface to CaptchaAlreadyAnsweredException \SocialvoidLib\Exceptions\Standard > Security

--- src/SocialvoidLib/Exceptions/Standard/Security/CaptchaAlreadyAnsweredException.php
+++ src/SocialvoidLib/Exceptions/Standard/Security/CaptchaAlreadyAnsweredException.php
@@ -4,9 +4,11 @@
 
     use Exception;
     use SocialvoidLib\Abstracts\StandardErrorCodes;
+    use SocialvoidLib\Interfaces\StandardErrorInterface;
+    use SocialvoidLib\Objects\Standard\ErrorDefinition;
     use Throwable;
 
-    class CaptchaAlreadyAnsweredException extends Exception
+    class CaptchaAlreadyAnsweredException extends Exception implements StandardErrorInterface
     {
         /**
          * @param string $message
@@ -15,5 +17,37 @@
         public function __construct($message = "Cannot answer a captcha that has already been answered", Throwable $previous = null)
         {
             parent::__construct($message, StandardErrorCodes::CaptchaAlreadyAnsweredException, $previous);
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getDefinition(): ErrorDefinition
+        {
+            return new ErrorDefinition(self::getName(), self::getDescription(), self::getErrorCode());
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getName(): string
+        {
+            return 'CaptchaAlreadyAnswered';
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getDescription(): string
+        {
+            return 'Raised when attempting to answer a captcha that has already been answered';
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getErrorCode(): int
+        {
+            return StandardErrorCodes::CaptchaAlreadyAnsweredException;
         }
     }
