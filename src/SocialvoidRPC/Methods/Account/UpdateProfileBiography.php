5c3dba20b87d15b6d002e40a6ad457fdc9f9fc3d
Netkas <zixing.narrakas1996@gmail.com>
2021-12-02 15:05:00 -0500

    Updated UpdateProfileBiography in \SocialvoidRPC\Methods\Account to implement StandardMethodInterface

--- src/SocialvoidRPC/Methods/Account/UpdateProfileBiography.php
+++ src/SocialvoidRPC/Methods/Account/UpdateProfileBiography.php
@@ -7,6 +7,8 @@
     use KimchiRPC\Interfaces\MethodInterface;
     use KimchiRPC\Objects\Request;
     use KimchiRPC\Objects\Response;
+    use SocialvoidLib\Abstracts\Flags\PermissionSets;
+    use SocialvoidLib\Abstracts\Types\BuiltinTypes;
     use SocialvoidLib\Classes\Validate;
     use SocialvoidLib\Exceptions\GenericInternal\CacheException;
     use SocialvoidLib\Exceptions\GenericInternal\DatabaseException;
@@ -16,17 +18,25 @@
     use SocialvoidLib\Exceptions\Standard\Authentication\NotAuthenticatedException;
     use SocialvoidLib\Exceptions\Standard\Authentication\SessionExpiredException;
     use SocialvoidLib\Exceptions\Standard\Authentication\SessionNotFoundException;
+    use SocialvoidLib\Exceptions\Standard\Network\AccessDeniedException;
     use SocialvoidLib\Exceptions\Standard\Network\DocumentNotFoundException;
     use SocialvoidLib\Exceptions\Standard\Network\PeerNotFoundException;
+    use SocialvoidLib\Exceptions\Standard\Security\InsufficientPermissionsException;
     use SocialvoidLib\Exceptions\Standard\Server\InternalServerException;
+    use SocialvoidLib\Exceptions\Standard\Validation\FileTooLargeException;
     use SocialvoidLib\Exceptions\Standard\Validation\InvalidBiographyException;
     use SocialvoidLib\Exceptions\Standard\Validation\InvalidClientPublicHashException;
+    use SocialvoidLib\Exceptions\Standard\Validation\InvalidFileForProfilePictureException;
     use SocialvoidLib\Exceptions\Standard\Validation\InvalidSessionIdentificationException;
+    use SocialvoidLib\Interfaces\StandardMethodInterface;
     use SocialvoidLib\NetworkSession;
+    use SocialvoidLib\Objects\Standard\MethodDefinition;
+    use SocialvoidLib\Objects\Standard\ParameterDefinition;
     use SocialvoidLib\Objects\Standard\SessionIdentification;
+    use SocialvoidLib\Objects\Standard\TypeDefinition;
     use SocialvoidRPC\SocialvoidRPC;
 
-    class UpdateProfileBiography implements MethodInterface
+    class UpdateProfileBiography implements MethodInterface, StandardMethodInterface
     {
 
         /**
@@ -61,6 +71,108 @@
             return '1.0.0.0';
         }
 
+        /**
+         * @inheritDoc
+         */
+        public static function getStandardNamespace(): string
+        {
+            return 'account';
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getStandardMethodName(): string
+        {
+            return 'update_profile_biography';
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getStandardMethod(): string
+        {
+            return self::getStandardNamespace() . '.' . self::getStandardMethodName();
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getStandardDescription(): string
+        {
+            return 'Updates the users profile biography';
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getStandardPossibleErrorCodes(): array
+        {
+            return [
+                BadSessionChallengeAnswerException::getErrorCode(),
+                InternalServerException::getErrorCode(),
+                InvalidClientPublicHashException::getErrorCode(),
+                InvalidSessionIdentificationException::getErrorCode(),
+                NotAuthenticatedException::getErrorCode(),
+                SessionExpiredException::getErrorCode(),
+                SessionExpiredException::getErrorCode(),
+                InvalidBiographyException::getErrorCode()
+            ];
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getReturnTypes(): array
+        {
+            return [
+                new TypeDefinition(BuiltinTypes::Boolean, false)
+            ];
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getStandardParameters(): array
+        {
+            return [
+                new ParameterDefinition('session_identification', [
+                    new TypeDefinition(SessionIdentification::getName()),
+                ], true, 'The Session Identification object'),
+                new ParameterDefinition('biography', [
+                    new TypeDefinition(BuiltinTypes::String)
+                ], true, 'The biography text to set to the users profile')
+            ];
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getStandardPermissionRequirements(): array
+        {
+            return [
+                PermissionSets::User,
+                PermissionSets::Proxy,
+                PermissionSets::Bot
+            ];
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public static function getDefinition(): MethodDefinition
+        {
+            return new MethodDefinition(
+                self::getStandardNamespace(),
+                self::getStandardMethodName(),
+                self::getStandardMethod(),
+                self::getStandardDescription(),
+                self::getStandardPermissionRequirements(),
+                self::getStandardParameters(),
+                self::getStandardPossibleErrorCodes(),
+                self::getReturnTypes()
+            );
+        }
 
         /**
          * @param Request $request
@@ -81,7 +193,6 @@
             if(gettype($request->Parameters["biography"]) !== "string")
                 throw new InvalidBiographyException("The 'biography' parameter must be a string", $request->Parameters["biography"]);
         }
-
 
         /**
          * @param Request $request
